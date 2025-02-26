946e105ada2d6e72e4445e0f7dc15358d58e4def
oywyfyly <jayden.desmond.brisbane@gmail.com>
2025-02-26 17:23:11 +1000

    new

--- script.js
+++ script.js
@@ -12,17 +12,29 @@ document.addEventListener('keydown', (e)=> {
         let activeMargin = parseFloat(mLeft);
         let prevMargin = parseFloat(window.getComputedStyle(document.activeElement.parentElement.previousElementSibling)?.marginLeft || "0px");
 
-        if (activeMargin - prevMargin < 60 && !e.shiftKey) {
-        document.activeElement.parentElement.style.marginLeft = `${activeMargin + 60}px`;
+        if (activeMargin - prevMargin < 100 && !e.shiftKey) {
+            document.activeElement.parentElement.style.marginLeft = `${activeMargin + 100}px`;
         }
         else if (activeMargin > 0 && e.shiftKey) {
-        document.activeElement.parentElement.style.marginLeft = `${Math.max(activeMargin - 60, 0)}px`;
+            document.activeElement.parentElement.style.marginLeft = `${Math.max(activeMargin - 100, 0)}px`;
         }
     }
 
     if (e.key === 'Enter') {
         newDiv = document.createElement('div')
-        newDiv.innerHTML = `<input id="node" placeholder="${divsBeforeActive}">`
+
+        newDiv.classList.add('node-container');  // Add the class for styling
+
+        newDiv.innerHTML = `
+        
+        <input id="node" placeholder="${divsBeforeActive}">
+        
+        <svg width="200" height="65" xmlns="http://www.w3.org/2000/svg" id="svg1">
+            <path
+                d="M 30 0 Q 40 60 120 60"
+                stroke="#008c00" stroke-width="5" fill="none" />
+        </svg>`
+        
         newDiv.style.marginLeft = mLeft
         newDiv.style.marginTop = `10px`
         document.getElementById('main').insertBefore(newDiv, document.activeElement.parentElement.nextElementSibling);
@@ -44,6 +56,20 @@ document.addEventListener('keydown', (e)=> {
 
     if (e.key === 'ArrowDown'){
         document.activeElement.parentElement.nextElementSibling?.querySelector('input')?.focus()
+    }
+
+    if (e.key === 'p' && e.ctrlKey){
+        document.activeElement.style.backgroundColor = '#b41414ff';
+    }
+
+    if (e.key === 's' && e.ctrlKey){
+        e.preventDefault()
+        document.activeElement.style.backgroundColor = '#008c00ff';
+    }
+
+    if (e.key === 'n' && e.ctrlKey){
+        e.preventDefault()
+        document.activeElement.style.backgroundColor = '#787878ff';
     }
 });
 
